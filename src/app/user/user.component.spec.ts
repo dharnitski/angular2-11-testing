de71493d9fb02a08bb802f721dd62ef9860bd074
Dmitry Harnitski <dharnitski@gmail.com>
2017-03-11 14:07:30 -0500

    test 'should display the user name if user is logged in'

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,9 +1,9 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -27,6 +27,14 @@ describe('UserComponent', () => {
   });
 
   it('should use the user name from the service', () => {
+    const userService = fixture.debugElement.injector.get(UserService);
+    expect(userService.user.name).toEqual(component.user.name);
+  });
 
+  it('should display the user name if user is logged in', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(component.user.name);
   });
 });
