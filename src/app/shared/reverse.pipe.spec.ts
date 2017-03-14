f77b5e7fbad0c843c869201c4fe38bb02cbbfe6c
Dmitry Harnitski <dharnitski@gmail.com>
2017-03-13 22:05:14 -0400

    add isolated test for pipe

---
+++ src/app/shared/reverse.pipe.spec.ts
@@ -0,0 +1,12 @@
+import { ReversePipe } from './reverse.pipe';
+
+
+describe('ReversePipe', () => {
+
+    it('should reverse text', () => {
+        const reversePipe = new ReversePipe();
+        expect(reversePipe.transform('hello')).toEqual('olleh');
+    });
+
+
+});
